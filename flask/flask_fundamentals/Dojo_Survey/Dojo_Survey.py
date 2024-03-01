32792145f151e7fade050445a6a783df953c9773
Balqees Awawdeh <balqisawawde@gmail.com>
2024-03-01 18:30:50 +0200

    Dojo Survey folder

---
+++ flask/flask_fundamentals/Dojo_Survey/Dojo_Survey.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+# our index route will handle rendering our form
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/result', methods=['POST'])
+def create_user():
+    print("Got Post Info")
+    print(request.form)
+    name = request.form['name']
+    location = request.form['location']
+    language=request.form['language']
+    comment=request.form['comment']
+    return render_template("results.html", name=name, location=location,language=language,comment=comment)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
